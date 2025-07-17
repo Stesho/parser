c20b5f7805ba206a49744ba97ed3b213513e3059
Dmitry Steshenko <d.steshenko@nota.tech>
2025-07-17 03:43:52 +0300

    feat: add stealth plugin

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+const URL =
+  "https://exnode.ru/exchange/tether_trc20_usdt-usdttrc-to-sberbank-sberrub";
+
+puppeteer.use(StealthPlugin());
 
 const fetchRate = async () => {
-  const url =
-    "https://exnode.ru/exchange/tether_trc20_usdt-usdttrc-to-sberbank-sberrub";
-
   const browser = await puppeteer.launch({
     headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
-  );
-  await page.setViewport({ width: 1366, height: 768 });
-  await page.goto(url, {
-    waitUntil: ["domcontentloaded"],
-  });
 
-  await page.screenshot({ path: "debug.png", fullPage: true });
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.goto(URL, {
+    waitUntil: "domcontentloaded",
+  });
 
   await page.waitForSelector("table tbody tr td:nth-child(4)");
 
@@ -26,12 +25,10 @@ const fetchRate = async () => {
     "table tbody tr td:nth-child(4)",
     (divs) => divs.map((div) => div.firstChild.textContent)
   );
-  console.log("Собранные данные о курсах:");
+
   console.log(divsCounts[0]);
 
   await browser.close();
 };
 
-(async () => {
-  await fetchRate();
-})();
+setInterval(fetchRate, 30_000);

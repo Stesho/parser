375e296e5a63a75ebabccf0c0d2edf40235c3212
Dmitry Steshenko <d.steshenko@nota.tech>
2025-07-19 19:28:40 +0300

    feat: add write to file

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-
-const URL =
-  "https://exnode.ru/exchange/tether_trc20_usdt-usdttrc-to-sberbank-sberrub";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
 puppeteer.use(StealthPlugin());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const FILE_PATH = path.join(__dirname, "rates.txt");
+const URL =
+  "https://exnode.ru/exchange/tether_trc20_usdt-usdttrc-to-sberbank-sberrub";
+
 const fetchRate = async () => {
   const browser = await puppeteer.launch({
-    headless: "new",
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
@@ -21,14 +27,24 @@ const fetchRate = async () => {
 
   await page.waitForSelector("table tbody tr td:nth-child(4)");
 
-  const divsCounts = await page.$$eval(
-    "table tbody tr td:nth-child(4)",
-    (divs) => divs.map((div) => div.firstChild.textContent)
+  const rates = await page.$$eval("table tbody tr td:nth-child(4)", (tds) =>
+    tds.map((td) => td.firstChild.textContent)
   );
 
-  console.log(divsCounts[0]);
+  const bestRate = rates[0];
+  const log = `[${new Date().toLocaleTimeString()}]: ${bestRate}`;
+  const data = [
+    `USDTTRC20 - SBERRUB : ${bestRate} + 0.001`,
+    `SBERRUB - USDTTRC20 : (USDTTRC20 - SBERRUB)`,
+  ].join("\n");
 
+  console.log(log);
+
+  await fs.writeFile(FILE_PATH, data, "utf-8");
   await browser.close();
 };
 
-setInterval(fetchRate, 30_000);
+(async () => {
+  await fetchRate();
+  setInterval(fetchRate, 10_000);
+})();

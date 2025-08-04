1c3f8ede3d65f1c4429c590c0305504bedf93998
Dmitri Stsiashenka <dmitri.stsiashenka@modsen-software.com>
2025-08-04 06:13:31 +0300

    feat: setup to create docker image

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import express from "express";
 import fs from "fs/promises";
+import minimist from 'minimist';
 
 puppeteer.use(StealthPlugin());
 
-const FILE_PATH = "/app/data/rates.txt";;
-const URL =
-  "https://exnode.ru/exchange/tether_trc20_usdt-usdttrc-to-sberbank-sberrub";
+const args = minimist(process.argv.slice(2));
 
-const proxyList = [
-  "adkgwmfT:uGYhU2yM@45.145.91.110:63538", 
-  "adkgwmfT:uGYhU2yM@193.58.170.58:64392", 
-  "adkgwmfT:uGYhU2yM@45.128.129.16:62588", 
-  "adkgwmfT:uGYhU2yM@92.249.15.219:62810", 
-  "adkgwmfT:uGYhU2yM@194.226.112.218:64898", 
-  "adkgwmfT:uGYhU2yM@193.232.113.240:63700", 
-  "adkgwmfT:uGYhU2yM@194.226.20.233:62258", 
-  "adkgwmfT:uGYhU2yM@195.208.82.166:64544", 
-  "adkgwmfT:uGYhU2yM@195.208.93.30:64454", 
-  "adkgwmfT:uGYhU2yM@154.211.18.212:64408",
-];
+const URL = args.url;
+const FILE_PATH = `/app/data/${args.file}`;
+const PAIR = args.pair.split('-') || ['USDTTRC20', 'SBERRUB'];
+const PROXY_LIST = args.proxies.split(",");
 
 let browser;
 let page;
 let currentProxyIndex = 0;
 
 const getNextProxy = () => {
-  const proxy = proxyList[currentProxyIndex % proxyList.length];
+  const proxy = PROXY_LIST[currentProxyIndex % PROXY_LIST.length];
   currentProxyIndex++;
   return proxy;
 };
@@ -82,17 +72,22 @@ async function scrape() {
     const bestExchange = names[0];
     let bestRate = rates[0];
     const log = `[${new Date().toLocaleTimeString()}]: ${bestExchange} ${bestRate}`;
+
+    console.log(log);
+
+    if(bestRate === '0') {
+      return;
+    }
     
     if(bestExchange === 'VibeBit') {
       bestRate = rates[1];
     }
     
+    const [from, to] = PAIR;
     const data = [
-      `USDTTRC20 - SBERRUB : ${bestRate} + 0.0006`,
-      `SBERRUB - USDTTRC20 : (USDTTRC20 - SBERRUB)`,
+      `${from} - ${to} : ${bestRate} + 0.0006`,
+      `${to} - ${from} : (${from} - ${to})`,
     ].join("\n");
-
-    console.log(log);
 
     await fs.writeFile(FILE_PATH, data, "utf-8");
   } catch(err) {
@@ -118,14 +113,3 @@ async function loop() {
   await init();
   await loop();
 })();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/rates.txt", (req, res) => {
-  res.sendFile(FILE_PATH);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server start on port ${PORT}`);
-});
